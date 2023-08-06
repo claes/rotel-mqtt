0a43329856f4a6183a6be5b1c13310927934c2ef
claes <claes@holmerson.net>
2023-08-06 21:10:17 +0200

    Added initial serial handling

--- go.mod
+++ go.mod
@@ -1,3 +1,12 @@
 module github.com/claes/rotel-mqtt
 
 go 1.19
+
+require (
+	github.com/eclipse/paho.mqtt.golang v1.4.3 // indirect
+	github.com/gorilla/websocket v1.5.0 // indirect
+	github.com/tarm/serial v0.0.0-20180830185346-98f6abe2eb07 // indirect
+	golang.org/x/net v0.8.0 // indirect
+	golang.org/x/sync v0.1.0 // indirect
+	golang.org/x/sys v0.6.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,9 +1,125 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"strconv"
 	"strings"
+
+	"github.com/tarm/serial"
 )
+
+// // MQTT bridge
+
+// type MQTTBridge struct {
+// 	MQTTClient mqtt.Client
+// }
+
+// func NewMQTTBridge() *MQTTBridge {
+// 	opts := mqtt.NewClientOptions().AddBroker("tcp://localhost:1883")
+// 	client := mqtt.NewClient(opts)
+// 	if token := client.Connect(); token.Wait() && token.Error() != nil {
+// 		panic(token.Error())
+// 	}
+
+// 	return &MQTTBridge{
+// 		MQTTClient : client,
+// 	}
+// }
+
+// func (mb *MQTTBridge) Publish(message string) {
+
+// 	for {
+// 		//currentTime := time.Now().Format(time.RFC3339)
+// 		token := client.Publish("time/topic", 0, false, message)
+// 		token.Wait()
+// 		fmt.Println("Published:", currentTime)
+// 		time.Sleep(1 * time.Second)
+// 	}
+// }
+
+// Rotel device
+
+type RotelDevice struct {
+	rotelDataParser RotelDataParser
+	Volume          string
+	Source          string
+	Freq            string
+	Mute            string
+	State           string
+	Display         string
+}
+
+func NewRotelDevice() *RotelDevice {
+	return &RotelDevice{
+		rotelDataParser: *NewRotelDataParser(),
+	}
+}
+
+func (rd *RotelDevice) SerialLoop() {
+	c := &serial.Config{Name: "/dev/ttyUSB0", Baud: 115200}
+	s, err := serial.OpenPort(c)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	buf := make([]byte, 128)
+	for {
+		n, err := s.Read(buf)
+		if err != nil {
+			log.Fatal(err)
+		}
+		//fmt.Print(string(buf[:n]))
+		rd.ProcessData(string(buf[:n]))
+		fmt.Printf("Volume: %s\n", rd.Volume)
+		fmt.Printf("Source: %s\n", rd.Source)
+		fmt.Printf("Freq: %s\n", rd.Freq)
+		fmt.Printf("Display: %s\n", rd.Display)
+		fmt.Printf("Len Display: %d\n", len(rd.Display))
+		fmt.Printf("State: %s\n", rd.State)
+		fmt.Printf("Mute: %s\n", rd.Mute)
+		fmt.Printf("\n")
+	}
+}
+
+//var serialMutex sync.Mutex
+
+func (rd *RotelDevice) SendRequest(message string) {
+	// serialMutex.Lock()
+	// defer serialMutex.Unlock()
+
+	// send
+}
+
+func (rd *RotelDevice) ProcessData(data string) {
+	rd.rotelDataParser.HandleParsedData(data)
+	for cmd := rd.rotelDataParser.GetNextRotelData(); cmd != nil; cmd = rd.rotelDataParser.GetNextRotelData() {
+
+		switch action := cmd[0]; action {
+		case "volume":
+			rd.Volume = cmd[1]
+		case "source":
+			rd.Source = cmd[1]
+		case "freq":
+			rd.Freq = cmd[1]
+		case "display":
+			rd.Display = cmd[1]
+		case "mute":
+			if cmd[1] == "on/off" {
+				rd.SendRequest("get_volume!")
+			} else {
+				rd.Mute = cmd[1]
+			}
+		case "power":
+			if cmd[1] == "on/standby" {
+				rd.SendRequest("get_power!")
+			} else {
+				rd.State = cmd[1]
+			}
+		}
+	}
+
+}
 
 // Rotel data parser
 
@@ -67,4 +183,25 @@ func (rdp *RotelDataParser) HandleParsedData(data string) {
 			rdp.NextKeyValuePair += string(c)
 		}
 	}
+}
+
+func main() {
+
+	rotelDevice := NewRotelDevice()
+	rotelDevice.SerialLoop()
+
+	// c := &serial.Config{Name: "/dev/ttyUSB0", Baud: 115200}
+	// s, err := serial.OpenPort(c)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	// buf := make([]byte, 128)
+	// for {
+	// 	n, err := s.Read(buf)
+	// 	if err != nil {
+	// 		log.Fatal(err)
+	// 	}
+	// 	fmt.Print(string(buf[:n]))
+	// }
 }
