bf539bbf5e84a231faf4ec79fcf03fca4ce42d4e
claes <claes@holmerson.net>
2024-06-06 14:50:25 +0200

    WIP new protocol parsing

--- lib/lib.go
+++ lib/lib.go
@@ -3,8 +3,6 @@ package lib
 import (
 	"encoding/json"
 	"log/slog"
-	"strconv"
-	"strings"
 	"sync"
 	"time"
 
@@ -229,71 +227,156 @@ func (bridge *RotelMQTTBridge) ProcessRotelData(data string) {
 
 // Rotel data parser
 
-type RotelDataParser struct {
-	RotelDataQueue   [][]string
-	NextKeyValuePair string
-}
+// type RotelDataParser struct {
+// 	RotelDataQueue   [][]string
+// 	NextKeyValuePair string
+// 	buffer           string
+// }
 
-func NewRotelDataParser() *RotelDataParser {
-	return &RotelDataParser{
-		RotelDataQueue:   [][]string{},
-		NextKeyValuePair: "",
-	}
-}
+// func NewRotelDataParser() *RotelDataParser {
+// 	return &RotelDataParser{
+// 		RotelDataQueue:   [][]string{},
+// 		NextKeyValuePair: "",
+// 	}
+// }
 
-func (rdp *RotelDataParser) GetNextRotelData() []string {
-	if len(rdp.RotelDataQueue) > 0 {
-		retVal := rdp.RotelDataQueue[0]
-		rdp.RotelDataQueue = rdp.RotelDataQueue[1:]
-		return retVal
-	} else {
-		return nil
-	}
-}
+// func (rdp *RotelDataParser) GetNextRotelData() []string {
+// 	if len(rdp.RotelDataQueue) > 0 {
+// 		retVal := rdp.RotelDataQueue[0]
+// 		rdp.RotelDataQueue = rdp.RotelDataQueue[1:]
+// 		return retVal
+// 	} else {
+// 		return nil
+// 	}
+// }
 
-func (rdp *RotelDataParser) PushKeyValuePair(keyValuePair string) {
-	keyValue := strings.Split(keyValuePair, "=")
-	rdp.RotelDataQueue = append(rdp.RotelDataQueue, keyValue)
-}
+// func (rdp *RotelDataParser) PushKeyValuePair(keyValuePair string) {
+// 	keyValue := strings.Split(strings.TrimSuffix(keyValuePair, "!"), "=")
+// 	rdp.RotelDataQueue = append(rdp.RotelDataQueue, keyValue)
+// }
 
-func (rdp *RotelDataParser) PushRotelData(rotelData []string) {
-	rdp.RotelDataQueue = append(rdp.RotelDataQueue, rotelData)
-}
+// func (rdp *RotelDataParser) PushRotelData(rotelData []string) {
+// 	rdp.RotelDataQueue = append(rdp.RotelDataQueue, rotelData)
+// }
 
-func (rdp *RotelDataParser) ComputeFixedLengthDataToRead(data string) int {
-	if strings.HasPrefix(data, "display=") && len(data) >= len("display=XXX") {
-		nextReadCount, _ := strconv.Atoi(data[len("display="):len("display=XXX")])
-		return nextReadCount
-	}
-	if strings.HasPrefix(data, "display1=") && len(data) >= len("display1=XX") {
-		nextReadCount, _ := strconv.Atoi(data[len("display1="):len("display1=XX")])
-		return nextReadCount
-	}
-	if strings.HasPrefix(data, "display2=") && len(data) >= len("display2=XX") {
-		nextReadCount, _ := strconv.Atoi(data[len("display2="):len("display2=XX")])
-		return nextReadCount
-	}
-	return 0
-}
+// func (rdp *RotelDataParser) ComputeFixedLengthDataToRead(data string) int {
+// 	if strings.HasPrefix(data, "display=") && len(data) >= len("display=XXX") {
+// 		nextReadCount, _ := strconv.Atoi(data[len("display="):len("display=XXX")])
+// 		return nextReadCount
+// 	}
+// 	if strings.HasPrefix(data, "display1=") && len(data) >= len("display1=XX") {
+// 		nextReadCount, _ := strconv.Atoi(data[len("display1="):len("display1=XX")])
+// 		return nextReadCount
+// 	}
+// 	if strings.HasPrefix(data, "display2=") && len(data) >= len("display2=XX") {
+// 		nextReadCount, _ := strconv.Atoi(data[len("display2="):len("display2=XX")])
+// 		return nextReadCount
+// 	}
+// 	return 0
+// }
 
-func (rdp *RotelDataParser) HandleParsedData(data string) {
-	for _, c := range data {
-		fixedLengthDataToRead := rdp.ComputeFixedLengthDataToRead(rdp.NextKeyValuePair)
-		if fixedLengthDataToRead > 0 {
-			s := rdp.NextKeyValuePair + string(c)
-			startIndex := len("display=XXX") + 1
-			if strings.HasPrefix(s, "display=") && (len(s)-startIndex) >= fixedLengthDataToRead {
-				value := s[startIndex : startIndex+fixedLengthDataToRead]
-				rdp.PushRotelData([]string{"display", value})
-				rdp.NextKeyValuePair = ""
-			} else {
-				rdp.NextKeyValuePair += string(c)
-			}
-		} else if "!" == string(c) {
-			rdp.PushKeyValuePair(rdp.NextKeyValuePair)
-			rdp.NextKeyValuePair = ""
-		} else {
-			rdp.NextKeyValuePair += string(c)
-		}
-	}
-}
+// func matchCommand(input string) bool {
+// 	return strings.Count(input, "=") == 1 &&
+// 		strings.HasSuffix(input, "!")
+// }
+
+// func matchDisplay(pattern, input string) *string {
+
+// 	pos := strings.Index(pattern, "=")
+// 	if pos == -1 {
+// 		return nil
+// 	}
+
+// 	prefix := pattern[:pos+1]
+// 	lengthFormat := pattern[pos+1:]
+
+// 	startIndex := strings.Index(input, prefix)
+// 	if startIndex == -1 {
+// 		return nil
+// 	}
+
+// 	numberStartIndex := startIndex + len(prefix)
+
+// 	if numberStartIndex+len(lengthFormat) > len(input) {
+// 		return nil
+// 	}
+
+// 	numberStr := input[numberStartIndex : numberStartIndex+len(lengthFormat)]
+
+// 	count, err := strconv.Atoi(numberStr)
+// 	if err != nil {
+// 		return nil
+// 	}
+
+// 	substringStartIndex := numberStartIndex + len(lengthFormat) + 1 //1 for comma
+
+// 	substringEndIndex := substringStartIndex + count
+// 	if substringEndIndex > len(input) {
+// 		return nil
+// 	}
+
+// 	result := input[substringStartIndex:substringEndIndex]
+// 	return &result
+// }
+
+// func (rdp *RotelDataParser) match(s string) (bool, string) {
+
+// 	if matchCommand(s) {
+// 		return true, s
+// 	} else {
+// 		displayMatch := matchDisplay("display1=XX", s)
+// 		if displayMatch != nil {
+// 			return true, "display=" + *displayMatch
+// 		}
+// 		displayMatch = matchDisplay("display2=XX", s)
+// 		if displayMatch != nil {
+// 			return true, "display=" + *displayMatch
+// 		}
+// 		displayMatch = matchDisplay("display=XXX", s)
+// 		if displayMatch != nil {
+// 			return true, "display=" + *displayMatch
+// 		}
+// 	}
+// 	return false, s
+// }
+
+// func (rdp *RotelDataParser) HandleParsedDataNew(data string) {
+// 	rdp.buffer += string(data)
+// 	next := ""
+// outer:
+// 	for len(rdp.buffer) > 0 {
+// 		for i, c := range rdp.buffer {
+// 			next += string(c)
+// 			match, matchedString := rdp.match(next)
+// 			if match {
+// 				rdp.PushKeyValuePair(matchedString)
+// 				next = ""
+// 				rdp.buffer = rdp.buffer[i+1:]
+// 				continue outer
+// 			}
+// 		}
+// 		break
+// 	}
+// }
+
+// func (rdp *RotelDataParser) HandleParsedData(data string) {
+// 	for _, c := range data {
+// 		fixedLengthDataToRead := rdp.ComputeFixedLengthDataToRead(rdp.NextKeyValuePair)
+// 		if fixedLengthDataToRead > 0 {
+// 			s := rdp.NextKeyValuePair + string(c)
+// 			startIndex := len("display=XXX") + 1
+// 			if strings.HasPrefix(s, "display=") && (len(s)-startIndex) >= fixedLengthDataToRead {
+// 				value := s[startIndex : startIndex+fixedLengthDataToRead]
+// 				rdp.PushRotelData([]string{"display", value})
+// 				rdp.NextKeyValuePair = ""
+// 			} else {
+// 				rdp.NextKeyValuePair += string(c)
+// 			}
+// 		} else if "!" == string(c) {
+// 			rdp.PushKeyValuePair(rdp.NextKeyValuePair)
+// 			rdp.NextKeyValuePair = ""
+// 		} else {
+// 			rdp.NextKeyValuePair += string(c)
+// 		}
+// 	}
+// }
--- lib/lib_test.go
+++ lib/lib_test.go
@@ -88,3 +88,61 @@ func TestMixed(t *testing.T) {
 		t.Error("Expected 'freq, 44.1', got ", rotelData)
 	}
 }
+
+func TestFoo(t *testing.T) {
+	r := NewRotelDataParser()
+	//r.HandleParsedData(" PCM    volume=39!source=opt1!fr")
+
+	r.HandleParsedData("power=on!")
+	found := false
+	for cmd := r.GetNextRotelData(); cmd != nil; cmd = r.GetNextRotelData() {
+		switch action := cmd[0]; action {
+		case "power":
+			found = true
+		}
+	}
+	if !found {
+		t.Error("Did not find power")
+	}
+}
+
+func TestDisplay(t *testing.T) {
+	r := matchDisplay("display1=XX", "display1=04,abcde")
+	if *r != "abcd" {
+		t.Error("Failed match, expected abcd, was " + *r)
+	}
+
+	r = matchDisplay("display1=XXX", "display1=004,abcde")
+	if *r != "abcd" {
+		t.Error("Failed match, expected abcd, was " + *r)
+	}
+
+}
+
+func TestTerminatedNew(t *testing.T) {
+	r := NewRotelDataParser()
+	r.HandleParsedDataNew("source=coax2!freq=44.1!")
+	r.HandleParsedDataNew("display=010,01234")
+	r.HandleParsedDataNew("56789A")
+	r.HandleParsedDataNew("display1=20,  COAX1      VOL 39 display2=20, BASS 0     TREB 0  volume=39!source=coax1!freq=off!tone=on!bass=000!treble=000!balance=000!display_update=auto!display=040,  COAX1      VOL 39  BASS 0     TREB 0  display1=20,  COAX1      VOL 39 display2=20, BASS 0     TREB 0  volume=39!source=coax1!freq=off!tone=on!bass=000!treble=000!balance=000!power=on!display_update=auto!display=040,  COAX1      VOL 39  BASS 0     TREB 0  display1=20,  COAX1      VOL 39 display2=20, BASS 0     TREB 0  volume=39!source=coax1!freq=off!tone=on!bass=000!treble=000!balance=000!power=on!display_update=auto!display=040,  COAX1      VOL 39  BASS 0     TREB 0  display1=20,  COAX1      VOL 39 display2=20, BASS 0     TREB 0  volume=39!source=coax1!freq=off!tone=on!bass=000!treble=000!balance=000!power=on!display_update=auto!display=040,  COAX1      VOL 39  BASS 0     TREB 0  display1=20,  COAX1      VOL 39 display2=20, BASS 0     TREB 0  volume=39!source=coax1!freq=off!tone=on!bass=000!treble=000!balance=000!power=on!display_update=auto!display=040,  COAX1      VOL 39  BASS 0     TREB 0  display1=20,  COAX1      VOL 39 display2=20, BASS 0     TREB 0  volume=39!source=coax1!freq=off!tone=on!bass=000!treble=000!balance=000!")
+
+	rotelData := r.GetNextRotelData()
+	if rotelData[0] != "source" || rotelData[1] != "coax2" {
+		t.Error("Expected 'source, coax2', got ", rotelData)
+	}
+
+	rotelData = r.GetNextRotelData()
+	if rotelData[0] != "freq" || rotelData[1] != "44.1" {
+		t.Error("Expected 'freq, 44.1', got ", rotelData)
+	}
+
+	rotelData = r.GetNextRotelData()
+	if rotelData[0] != "display" || rotelData[1] != "0123456789" {
+		t.Error("Expected 'display, 0123456789', got ", rotelData)
+	}
+	rotelData = r.GetNextRotelData()
+	if rotelData[0] != "display1" || rotelData[1] != "  COAX1      VOL 39 " {
+		t.Error("Expected 'display1,   COAX1      VOL 39 ', got ", rotelData)
+	}
+
+}
